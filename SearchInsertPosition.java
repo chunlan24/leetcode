5a08354ca6d5b4aaf8621d364cc45fb7a31148b4
Chunlan Han <Chunlan Han>
2016-12-11 00:03:51 +0800

    Add search insert position

---
+++ SearchInsertPosition.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public int searchInsert(int[] A, int target) {
+        
+        if (A == null || A.length == 0)
+        {
+            return 0;
+        }
+        
+        int l = 0;
+        int r = A.length - 1;
+        
+        while (l <= r)
+        {
+            int m = (l + r) / 2;
+            if (A[m] == target)
+            {
+                return m;
+            } else if (A[m] < target) {
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+        return l;
+    }
+}
