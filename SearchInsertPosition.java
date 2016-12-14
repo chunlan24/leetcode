036e3f7e253e532316753682e093d146ab5f6ae5
chunlan24 <chunlan24@hotmail.com>
2016-12-15 00:16:58 +0800

    add test for SearchInsertPosition

--- SearchInsertPosition.java
+++ SearchInsertPosition.java
@@ -1,4 +1,5 @@
 public class SearchInsertPosition {
+    
     public int searchInsert(int[] A, int target) {
         
         if (A == null || A.length == 0)
@@ -22,5 +23,15 @@ public class SearchInsertPosition {
             }
         }
         return l;
+    }  
+
+    public static void main (String[] args) {
+
+        int[] array = {1,3,5,6};
+        // Arrays.sort(array);
+        SearchInsertPosition find = new SearchInsertPosition();
+        System.out.println(find.searchInsert(array,5));
+        System.out.println(find.searchInsert(array,9));
+        System.out.println(find.searchInsert(array,2));
     }
 }
